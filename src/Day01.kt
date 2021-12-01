1581ae71d6a899d8acae7329adb5cf85ea6bf200
Ranveer Aggarwal <ranveeraggarwal@gmail.com>
2021-12-02 00:49:44 +0530

    Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,10 +1,22 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var larger = 0
+        for (i in 1 until input.size) {
+            if (input[i].toInt() > input[i-1].toInt()) {
+                larger += 1
+            }
+        }
+        return larger
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var larger = 0
+        for (i in 3 until input.size) {
+            if (input[i-3].toInt() + input[i-2].toInt() + input[i-1].toInt() < input[i-2].toInt() + input[i-1].toInt() + input[i].toInt()) {
+                larger += 1
+            }
+        }
+        return larger
     }
 
     // test if implementation meets criteria from the description, like:
